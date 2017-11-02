e8775ebe470a5979440a1c7d21e55335edd1df96
Justin Theiss <jdeantheiss@gmail.com>
2017-11-02 11:06:37 -0700

    Updated with setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='auditory_prf_task',
+      version='0.1',
+      author='Justin Theiss',
+      py_modules=['auditory_pRF','equate_freqs','generate_wave'],
+     )
